64f0d7c106eee7af7be68cd058329458a58cdcdb
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-08 05:54:34 -0500

    add user component as a reusable component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, computed, Input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,19 +8,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  imagePath = computed(() => {
-    return 'assets/users/' + this.selectedUser().avatar;
-  });
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser().avatar;
-  // }
-
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    // this.selectedUser = DUMMY_USERS[randomIndex];
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onSelectUser() {}
 }
