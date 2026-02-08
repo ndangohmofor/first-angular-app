17cd312d68f86b4df8712803b9a2bf41b0440e70
I M Ndangoh <ndangohmofor@gmail.com>
2026-02-08 09:23:39 -0500

    add task component and pass the name to it

---
+++ src/app/task/task.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.html',
+  styleUrl: './task.css',
+})
+export class TaskComponent {
+  @Input({ required: true }) name!: string;
+}
