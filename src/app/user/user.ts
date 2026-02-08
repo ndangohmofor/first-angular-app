2534904c2b085a416f5683b2be6980c6a5bae2b5
I M Ndangoh <ndangohmofor@gmail.com>
2026-02-08 08:44:18 -0500

    using the output function to implement an event emitter

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,7 +12,9 @@ export class UserComponent {
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
 
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+
+  select = output<string>();
 
   // avatar = input.required<string>();
   // name = input.required<string>();
