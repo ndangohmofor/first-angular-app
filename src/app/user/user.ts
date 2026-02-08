5cd805457f1858f1b60e1bc6bb9cdd5a25dce2ca
I M Ndangoh <ndangohmofor@gmail.com>
2026-02-08 08:18:57 -0500

    use Output decorator to emit events and handle them in another component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,8 +8,11 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.css',
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+
+  @Output() select = new EventEmitter();
 
   // avatar = input.required<string>();
   // name = input.required<string>();
@@ -22,5 +25,7 @@ export class UserComponent {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
