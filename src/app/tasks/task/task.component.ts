3dc1968e0009b014668fa7eec5c52e67b46a1b9a
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-09 07:03:55 -0500

    display the details of each users tasks on the UI

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+interface ITask {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 @Component({
   selector: 'app-task',
   imports: [],
@@ -7,5 +14,5 @@ import { Component } from '@angular/core';
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
-
+  @Input({ required: true }) task!: ITask;
 }
