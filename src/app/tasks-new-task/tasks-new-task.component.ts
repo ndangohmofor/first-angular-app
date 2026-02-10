ff56e1c51e109e73c4bc3753dd50cdb59822ca69
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-10 06:04:08 -0500

    add component for new task

---
+++ src/app/tasks-new-task/tasks-new-task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-new-task',
+  imports: [],
+  templateUrl: './tasks-new-task.component.html',
+  styleUrl: './tasks-new-task.component.css',
+})
+export class TasksNewTaskComponent {
+
+}
