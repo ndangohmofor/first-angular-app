997016eed04975a93eae788deeaa86f38991554e
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-16 09:45:44 -0500

    make the task service injectable

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@angular/core';
 import { ITaskData } from './task/task.model';
 
+@Injectable({ providedIn: 'root' })
 export class TasksService {
   private tasks = [
     {
