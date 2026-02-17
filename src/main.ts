b11c4b4cf643f3d91157eba3c14cdf72f01be53a
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-17 05:49:05 -0500

    update main to bootstrap the app module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModule } from './app/app.module';
 
-import { AppComponent } from './app/app.component';
+platformBrowser().bootstrapModule(AppModule);
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
