495327eab4ed6304336439231fed6154e4de2e48
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-17 06:04:47 -0500

    import stand alone components in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
-  imports: [],
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
   exports: [],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
